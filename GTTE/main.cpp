72ec205affadba476d3fc45cf2f4556dd4a909c4
nicoboss <nico@bosshome.ch>
2014-10-17 02:41:14 +0200

    Version of Wednesday noon

--- GTTE/main.cpp
+++ GTTE/main.cpp
@@ -25,11 +25,11 @@
 #define LIGHTMAGENTA 13
 #define YELLOW 14
 #define WHITE 15
-#define BLINK 12
+#define BLINK 128
 
 
 //#include <P:\winmm.h>
-
+#pragma comment(lib, "user32")
 
 using namespace std;
 
@@ -39,15 +39,15 @@ using namespace std;
 
 string Spielfeld[3][10] {
     {
-        {"XXXXXXXXXXXXXXXXXXXXXXXX\n"},
-        {"X  X   X$X         X\n"},
-        {"X XX X X XX    XX XX\n"},
-        {"X      X       X        X\n"},
-        {"XXXXXX X XX    XX XX\n"},
-        {"X    X   X         X\n"},
-        {"X?XX   XXXXXXXXXXXXXXXX\n"},
-        {"X    XXXXXXXXXXXXXXXXX\n"},
-        {"XXXXXXXXXXXXXXXXXXXXX\n"}
+        {"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n"},
+        {"X  X   X$X         XXX            XX     XX\n"},
+        {"X XX X X XX    XX XXXX XXX       XXX XXX XX\n"},
+        {"X      X       X        XXX     XX     X XX\n"},
+        {"XXXXXX X XX    XX XXXXXXXXXX   XXX  X  X   \n"},
+        {"X    X   X         XXXXXXXXX   XXX     X XX\n"},
+        {"X?XX   XX$XXXXXXXXXXXXXXXXXXX XXXXXX XXX ?X\n"},
+        {"X    XXX                              XX XX\n"},
+        {"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n"}
     },
 
     {
@@ -70,14 +70,16 @@ string Spielfeld[3][10] {
 
 vector<vector<vector<vector<unsigned int>>>> Feind{
 {
-    {{6, 3}, {5, 3}, {4, 3},{3, 3},{2, 3},{1, 3}},
-    {{6, 1}, {6, 2}, {6, 3},{6, 4}, {6, 5}, {6, 6}},
-    {{1, 5},{2, 5}, {3, 5},{4, 5}, {4, 6}, {4, 7}, {3, 7},{2, 7},{1, 7}},
-    {{8, 3}, {9, 3}, {10, 3},{11, 3},{12, 3},{13, 3},{14, 3}},
-    {{12, 1}, {12, 2}, {12, 3},{12, 4},{12, 5}},
-    {{10, 1}, {11, 1}, {12, 1},{13, 1},{14, 1},{15, 1},{16, 1},{17, 1},{18, 1}},
-    {{10, 5}, {11, 5}, {12, 5},{13, 5},{14, 5},{15, 5},{16, 5},{17, 5},{18, 5}},
-    //{{0, 0}, {0, 0}, {0, 0},{0, 0}},
+    {{6, 3},{5, 3},{4, 3},{3, 3},{2, 3},{1, 3}},
+    {{6, 1},{6, 2},{6, 3},{6, 4},{6, 5},{6, 6}},
+    {{1, 5},{2, 5},{3, 5},{4, 5},{4, 6},{4, 7},{3, 7},{2, 7},{1, 7}},
+    {{8, 3},{9, 3},{10, 3},{11, 3},{12, 3},{13, 3},{14, 3}},
+    {{12, 1},{12, 2},{12, 3},{12, 4},{12, 5}},
+    {{10, 1},{11, 1},{12, 1},{13, 1},{14, 1},{15, 1},{16, 1},{17, 1},{18, 1}},
+    {{10, 5},{11, 5},{12, 5},{13, 5},{14, 5},{15, 5},{16, 5},{17, 5},{18, 5}},
+    {{16, 3},{17, 3},{18, 3},{19, 3},{20, 3},{21, 3},{22, 3},{23, 3}},
+    {{8, 7},{9, 7},{10, 7},{11, 7},{12, 7},{13, 7},{14, 7},{15, 7},{16, 7},{17, 7},{18, 7},{19, 7},{20, 7},{21, 7},{22, 7},
+    {23, 7},{24, 7},{25, 7},{26, 7},{27, 7},{28, 7},{29, 7},{30, 7},{31, 7},{32, 7},{33, 7},{34, 7},{35, 7},{36, 7},{37, 7}},
 },
 {
     {{0, 0}, {0, 0}, {0, 0},{0, 0}},
@@ -121,6 +123,14 @@ unsigned char Spielfigur_y_alt = 1;
 int i;
 
 
+
+void coutc(int fcolor, int bcolor, char output)
+{
+   HANDLE handle= GetStdHandle(STD_OUTPUT_HANDLE);
+   SetConsoleTextAttribute( handle, (fcolor + (bcolor * 16)) );
+   cout<< output;
+   //SetConsoleTextAttribute( handle, color| 15);
+}
 
 void gotoXY(int x, int y) {
 	HANDLE hStdout;
@@ -167,7 +177,6 @@ bool SpielfigurSetPos(unsigned char x, unsigned char y)
 
         }
     }
-
     if(Spielfeld[Level][y].at(x)==32)
     {
         Spielfeld[Level][Spielfigur_y_alt].at(Spielfigur_x_alt)=32;
@@ -223,7 +232,10 @@ for(size_t i=0; i < Feind[Level].size(); i++)
         FeindPorM[Level][i]=-1;
     }
 
-    Spielfeld[Level][FeindYPosAlt[Level][i]].at(FeindXPosAlt[Level][i])=32;
+    if(FeindYPosAlt[Level][i]!=0||FeindXPosAlt[Level][i]!=0)
+    {
+        Spielfeld[Level][FeindYPosAlt[Level][i]].at(FeindXPosAlt[Level][i])=32;
+    }
 
     FeindXPos[Level][i]=Feind[Level][i][FeindVectorPos[Level][i]][0];
     FeindYPos[Level][i]=Feind[Level][i][FeindVectorPos[Level][i]][1];
@@ -265,7 +277,6 @@ void Statusanzeige()
 
 
 
-
 int main(int argc, char *argv[])
 {
 unsigned int Feind_Neuberechnung=0;
@@ -298,14 +309,13 @@ mciSendString("play MY_SND repeat",0,0,0);
 // FOREGROUND_YELLOW + FOREGROUND_INTENSITY
 //SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
 //FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);
-
-
+//SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE),CONSOLE_FULLSCREEN_MODE,0);
 
 system("color F0");
 
 // FOREGROUND_YELLOW + FOREGROUND_INTENSITY + BACKGROUND_RED + BACKGROUND_INTENSITY
-//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
-//FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | BACKGROUND_INTENSITY | BACKGROUND_RED);
+//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),enum Colors { blau=1, gruen, cyan, rot, violett, gelb, grau, dunkelgrau, hellblau, hellgrün, hellrot, hellviolett, hellgelb, weiss };
+//FOREGROUND_INTENSITY ::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);| FOREGROUND_RED | FOREGROUND_GREEN | BACKGROUND_INTENSITY | BACKGROUND_RED);
 
 //WinExec(HHH.c_str(),0);
 //ShellExecute(NULL, "open", "P:\\Programme\\mpg123\\mpg123.exe", "P:\\Programme\\mpg123\\mpg123\\8-bit.mp3", NULL, SW_SHOWNORMAL);
@@ -314,6 +324,8 @@ system("color F0");
 
 int Map_Zeilen;
 int Map_Buchstaben;
+
+Feind_Neuberechnung=Schwirigkeitsgrad-1;
 
 while(true)
 {
@@ -327,13 +339,35 @@ while(true)
     //    cout << Spielfeld[Level][i];
     //}
 
-    for(Map_Zeilen=0; Map_Zeilen < sizeof(Spielfeld)/sizeof(char); Map_Zeilen++)
-        for(Map_Buchstaben=0; Map_Buchstaben < 10; Map_Buchstaben++)
+    for(Map_Zeilen=0; Map_Zeilen < 9; Map_Zeilen++)
+    {
+        for(Map_Buchstaben=0; Map_Buchstaben < 43; Map_Buchstaben++)
         {
-            //cout << Spielfeld[Level][Map_Zeilen].at(Map_Buchstaben);
-            cout << Spielfeld[Level][Map_Zeilen];
+            switch(Spielfeld[Level][Map_Zeilen].at(Map_Buchstaben))
+            {
+                case 1 :
+                    coutc(MAGENTA, WHITE, char(1));
+                    break;
+                case 2 :
+                    coutc(BLUE, WHITE, char(2));
+                    break;
+                case 3 :
+                    coutc(LIGHTRED,WHITE, char(3));
+                    break;
+                case 36 :
+                    coutc(LIGHTCYAN,WHITE, char(36));
+                    break;
+                default : coutc(0,15,Spielfeld[Level][Map_Zeilen].at(Map_Buchstaben));
+            }
+
+            //cout << Spielfeld[Level][Map_Zeilen];
+
+
         }
+
         cout << endl;
+    }
+
 
 
 
